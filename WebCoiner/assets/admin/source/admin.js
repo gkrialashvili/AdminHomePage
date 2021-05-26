f698b18f16c947f6a13b23a0f41883abb1bc3f76
gkrialashvili <giorgikrialashvili@hotmail.com>
2021-05-27 01:13:14 +0400

    home page added

--- WebCoiner/assets/admin/source/admin.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("package-area").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "flex";
-});
-
-document.getElementById("close-modal").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "none";
-});
