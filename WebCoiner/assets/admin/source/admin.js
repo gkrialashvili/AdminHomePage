c95f8286073a7bedcb58bbcc91280c3f70ad1397
gkrialashvili <gkrialashvili@MARKETING_1>
2021-05-26 23:12:10 +0400

    Add project files.

---
+++ WebCoiner/assets/admin/source/admin.js
@@ -0,0 +1,7 @@
+document.getElementById("package-area").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "flex";
+});
+
+document.getElementById("close-modal").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "none";
+});
