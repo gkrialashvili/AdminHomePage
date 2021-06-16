ed9a88a89a74a70032c16483310b0d0cb9e15483
gkrialashvili <giorgi.krialashvili08b@geolab.edu.ge>
2021-06-17 03:05:16 +0400

    dadada

--- WebCoiner/assets/home/countdown.js
+++ WebCoiner/assets/home/countdown.js
@@ -11,16 +11,28 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-    document.getElementById("day").innerHTML = days;
-    document.getElementById("hour").innerHTML = hours;
-    document.getElementById("minute").innerHTML = minutes;
-    document.getElementById("second").innerHTML = seconds;
+    if (days < 10)
+        document.getElementById("day").innerHTML = "0" + days;
+    else
+        document.getElementById("day").innerHTML = days;
+    if (hours < 10)
+        document.getElementById("hour").innerHTML = "0" + hours;
+    else
+        document.getElementById("hour").innerHTML = hours;
+    if (minutes < 10)
+        document.getElementById("minute").innerHTML = "0" + minutes;
+    else
+        document.getElementById("minute").innerHTML = minutes;
+    if (seconds < 10)
+        document.getElementById("second").innerHTML = "0" + seconds;
+    else
+        document.getElementById("second").innerHTML = seconds;
 
   // If the count down is over, write some text
   if (distance < 0) {
